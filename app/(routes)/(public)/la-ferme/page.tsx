b4cf7502d9b6f01340fdbbf9ca0b5db389e117d5
Aurélien <aurelien.r35@gmail.com>
2024-05-12 18:43:01 +0200

    page ferme

--- app/(routes)/(public)/la-ferme/page.tsx
+++ app/(routes)/(public)/la-ferme/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { StickyScrollRevealDemo } from "./_components/scroll-reveal";
 
 const PageLaFerme = () => {
   return (
@@ -8,23 +7,54 @@ const PageLaFerme = () => {
         <h2 className="mb-8 text-center text-3xl font-bold">
           À propos de nous
         </h2>
-        <div className="flex flex-wrap items-center justify-center text-center md:text-left">
-          <div className="w-[80vw] p-4 lg:w-[40vw]">
-            <Image
-              src="/ferme.webp"
-              width={2936}
-              height={1168}
-              alt="Photo de la ferme"
-              className="o rounded-lg shadow-md"
-            />
-          </div>
-          <div className="w-[80vw] p-4 lg:w-[40vw]">
-            <p className="mb-4 text-lg text-gray-700">
+        <div className="grid grid-cols-1 items-center justify-center gap-4 text-center sm:grid-cols-2 ">
+          <Image
+            src="/ferme.webp"
+            width={2936}
+            height={1168}
+            alt="Photo de la ferme"
+            className=" rounded-lg  shadow-md "
+          />
+          <div className="space-y-2 text-base lg:text-lg ">
+            <p>
               {
                 " Fondée en 1992, notre ferme familiale est située au cœur de la              région de Normandie, où nous cultivons avec passion et respect de              l'environnement. Nos pratiques agricoles durables soutiennent la              biodiversité et préservent les ressources naturelles."
               }
             </p>
-            <p className="text-lg text-gray-700">
+            <p>
+              Nous nous engageons à fournir des produits frais et de haute
+              qualité directement de la ferme à votre table. Découvrez nos
+              légumes biologiques, nos fromages artisanaux et notre gamme de
+              produits faits maison.
+            </p>
+          </div>
+          <div className="space-y-2 text-base lg:text-lg ">
+            <p>
+              {
+                " Fondée en 1992, notre ferme familiale est située au cœur de la              région de Normandie, où nous cultivons avec passion et respect de              l'environnement. Nos pratiques agricoles durables soutiennent la              biodiversité et préservent les ressources naturelles."
+              }
+            </p>
+            <p>
+              Nous nous engageons à fournir des produits frais et de haute
+              qualité directement de la ferme à votre table. Découvrez nos
+              légumes biologiques, nos fromages artisanaux et notre gamme de
+              produits faits maison.
+            </p>
+          </div>
+          <Image
+            src="/vache.webp"
+            width={1200}
+            height={1600}
+            alt="Photo de la ferme"
+            className=" max-h-[50vh] w-auto justify-self-center rounded-lg  object-contain shadow-md sm:row-span-2 "
+          />
+          <div className="space-y-2 text-base lg:text-lg ">
+            <p>
+              {
+                " Fondée en 1992, notre ferme familiale est située au cœur de la              région de Normandie, où nous cultivons avec passion et respect de              l'environnement. Nos pratiques agricoles durables soutiennent la              biodiversité et préservent les ressources naturelles."
+              }
+            </p>
+            <p>
               Nous nous engageons à fournir des produits frais et de haute
               qualité directement de la ferme à votre table. Découvrez nos
               légumes biologiques, nos fromages artisanaux et notre gamme de
@@ -33,7 +63,6 @@ const PageLaFerme = () => {
           </div>
         </div>
       </div>
-      <StickyScrollRevealDemo />
     </section>
   );
 };
