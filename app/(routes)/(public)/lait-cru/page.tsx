50408108728effa7918b59fff46c7c5a1331f507
Aurélien <aurelien.r35@gmail.com>
2024-06-24 13:14:04 +0200

    test static

--- app/(routes)/(public)/lait-cru/page.tsx
+++ app/(routes)/(public)/lait-cru/page.tsx
@@ -1,114 +1,102 @@
-import  type { Metadata } from "next";
+import type { Metadata } from "next";
 import VacheChamps from "@/public/vache-champs2.webp";
 import BidonLait from "@/public/bidon-lait.jpg";
 import Veau from "@/public/veau.webp";
 import Image from "next/image";
+import { nanoid } from "nanoid";
 
 export const dynamic = "force-static";
 
 export async function generateMetadata(): Promise<Metadata> {
-  return {
-    title: "Lait Cru",
-    description:
-      "Nous nous engageons à produire du lait cru bio de la plus haute qualité, en adoptant une approche respectueuse de l’environnement et de nos animaux. Le lait cru est un produit non pasteurisé qui conserve tous ses nutriments et saveurs naturels. Pour assurer une qualité irréprochable, nous suivons des pratiques rigoureuses de gestion de l’élevage et de la traite. Nos vaches pâturent librement dans des prairies naturelles, ce qui leur permet de se nourrir d’herbes fraîches et variées, contribuant à un lait riche et savoureux. Nous surveillons attentivement leur santé et leur bien-être, garantissant ainsi un produit final sain et nutritif.",
-  };
+	return {
+		title: "Lait Cru",
+		description:
+			"Nous nous engageons à produire du lait cru bio de la plus haute qualité, en adoptant une approche respectueuse de l’environnement et de nos animaux. Le lait cru est un produit non pasteurisé qui conserve tous ses nutriments et saveurs naturels. Pour assurer une qualité irréprochable, nous suivons des pratiques rigoureuses de gestion de l’élevage et de la traite. Nos vaches pâturent librement dans des prairies naturelles, ce qui leur permet de se nourrir d’herbes fraîches et variées, contribuant à un lait riche et savoureux. Nous surveillons attentivement leur santé et leur bien-être, garantissant ainsi un produit final sain et nutritif.",
+	};
 }
 
 const LaitCruPage = () => {
-  return (
-    <section id="about" className="py-16">
-      <div className="container mx-auto px-4">
-        <h1 className="mb-8 text-center text-5xl font-bold">
-          La Démarche du Lait Cru
-        </h1>
-        <div className="grid grid-cols-1 items-center justify-center gap-4 text-center sm:grid-cols-2">
-          <Image
-            src={VacheChamps}
-            width={3175}
-            height={1662}
-            placeholder="blur"
-            alt="Vache champs"
-            className="mx-auto max-w-sm rounded-lg shadow-md sm:max-w-full md:max-w-sm"
-          />
-          <div className="space-y-2 text-base lg:text-lg">
-            <p>
-              Nous nous engageons à produire du lait cru bio de la plus haute
-              qualité, en adoptant une approche respectueuse de l’environnement
-              et de nos animaux. Le lait cru est un produit non pasteurisé qui
-              conserve tous ses nutriments et saveurs naturels. Pour assurer une
-              qualité irréprochable, nous suivons des pratiques rigoureuses de
-              gestion de l’élevage et de la traite.
-            </p>
-            <p>
-              Nos vaches pâturent librement dans des prairies naturelles, ce qui
-              leur permet de se nourrir d’herbes fraîches et variées,
-              contribuant à un lait riche et savoureux. Nous surveillons
-              attentivement leur santé et leur bien-être, garantissant ainsi un
-              produit final sain et nutritif.
-            </p>
-          </div>
+	return (
+		<section id="about" className="py-16">
+			<div className="container mx-auto px-4">
+				<h1 className="mb-8 text-center text-5xl font-bold">La Démarche du Lait Cru {nanoid()}</h1>
+				<div className="grid grid-cols-1 items-center justify-center gap-4 text-center sm:grid-cols-2">
+					<Image
+						src={VacheChamps}
+						width={3175}
+						height={1662}
+						placeholder="blur"
+						alt="Vache champs"
+						className="mx-auto max-w-sm rounded-lg shadow-md sm:max-w-full md:max-w-sm"
+					/>
+					<div className="space-y-2 text-base lg:text-lg">
+						<p>
+							Nous nous engageons à produire du lait cru bio de la plus haute qualité, en adoptant une approche
+							respectueuse de l’environnement et de nos animaux. Le lait cru est un produit non pasteurisé qui conserve
+							tous ses nutriments et saveurs naturels. Pour assurer une qualité irréprochable, nous suivons des
+							pratiques rigoureuses de gestion de l’élevage et de la traite.
+						</p>
+						<p>
+							Nos vaches pâturent librement dans des prairies naturelles, ce qui leur permet de se nourrir d’herbes
+							fraîches et variées, contribuant à un lait riche et savoureux. Nous surveillons attentivement leur santé
+							et leur bien-être, garantissant ainsi un produit final sain et nutritif.
+						</p>
+					</div>
 
-          <div className="space-y-2 text-base lg:text-lg">
-            <p>
-              La production de lait cru nécessite un soin particulier durant le
-              processus de traite et de stockage pour prévenir toute
-              contamination. Nous utilisons des équipements de traite modernes
-              et désinfectés régulièrement pour maintenir des conditions
-              d’hygiène optimale.
-            </p>
-            <p>
-              Le lait est ensuite refroidi rapidement après la traite pour
-              préserver sa fraîcheur et ses qualités nutritives. Ce lait cru est
-              emballé et distribué directement aux consommateurs sans subir de
-              traitements thermiques, conservant ainsi tous les enzymes et
-              probiotiques bénéfiques qui peuvent être détruits lors de la
-              pasteurisation.
-            </p>
-          </div>
+					<div className="space-y-2 text-base lg:text-lg">
+						<p>
+							La production de lait cru nécessite un soin particulier durant le processus de traite et de stockage pour
+							prévenir toute contamination. Nous utilisons des équipements de traite modernes et désinfectés
+							régulièrement pour maintenir des conditions d’hygiène optimale.
+						</p>
+						<p>
+							Le lait est ensuite refroidi rapidement après la traite pour préserver sa fraîcheur et ses qualités
+							nutritives. Ce lait cru est emballé et distribué directement aux consommateurs sans subir de traitements
+							thermiques, conservant ainsi tous les enzymes et probiotiques bénéfiques qui peuvent être détruits lors de
+							la pasteurisation.
+						</p>
+					</div>
 
-          <Image
-            src={BidonLait}
-            width={500}
-            height={500}
-            placeholder="blur"
-            alt="Bidon lait"
-            className="mx-auto max-w-sm rounded-lg shadow-md sm:max-w-full md:max-w-sm"
-          />
-          <Image
-            src={Veau}
-            width={5184}
-            height={3456}
-            placeholder="blur"
-            alt="Veau"
-            className="mx-auto hidden max-w-sm rounded-lg shadow-md sm:block sm:max-w-full md:max-w-sm"
-          />
-          <div className="space-y-2 text-base lg:text-lg">
-            <p>
-              Notre démarche repose également sur une transparence totale et une
-              relation de confiance avec nos clients. Nous invitons les
-              consommateurs à visiter notre ferme pour découvrir nos méthodes de
-              production et rencontrer nos vaches. En achetant notre lait cru
-              bio, vous soutenez une agriculture durable et locale, tout en
-              profitant d’un produit authentique et savoureux.
-            </p>
-            <p>
-              Nous croyons fermement que le lait cru, lorsqu’il est produit dans
-              des conditions rigoureuses et respectueuses, est non seulement
-              bénéfique pour la santé, mais aussi pour l’environnement et
-              l’économie locale.
-            </p>
-          </div>
-          <Image
-            src={Veau}
-            width={5184}
-            height={3456}
-            placeholder="blur"
-            alt="Veau"
-            className="mx-auto max-w-sm rounded-lg shadow-md sm:hidden sm:max-w-full md:max-w-sm"
-          />
-        </div>
-      </div>
-    </section>
-  );
+					<Image
+						src={BidonLait}
+						width={500}
+						height={500}
+						placeholder="blur"
+						alt="Bidon lait"
+						className="mx-auto max-w-sm rounded-lg shadow-md sm:max-w-full md:max-w-sm"
+					/>
+					<Image
+						src={Veau}
+						width={5184}
+						height={3456}
+						placeholder="blur"
+						alt="Veau"
+						className="mx-auto hidden max-w-sm rounded-lg shadow-md sm:block sm:max-w-full md:max-w-sm"
+					/>
+					<div className="space-y-2 text-base lg:text-lg">
+						<p>
+							Notre démarche repose également sur une transparence totale et une relation de confiance avec nos clients.
+							Nous invitons les consommateurs à visiter notre ferme pour découvrir nos méthodes de production et
+							rencontrer nos vaches. En achetant notre lait cru bio, vous soutenez une agriculture durable et locale,
+							tout en profitant d’un produit authentique et savoureux.
+						</p>
+						<p>
+							Nous croyons fermement que le lait cru, lorsqu’il est produit dans des conditions rigoureuses et
+							respectueuses, est non seulement bénéfique pour la santé, mais aussi pour l’environnement et l’économie
+							locale.
+						</p>
+					</div>
+					<Image
+						src={Veau}
+						width={5184}
+						height={3456}
+						placeholder="blur"
+						alt="Veau"
+						className="mx-auto max-w-sm rounded-lg shadow-md sm:hidden sm:max-w-full md:max-w-sm"
+					/>
+				</div>
+			</div>
+		</section>
+	);
 };
 export default LaitCruPage;
--- app/(routes)/(public)/panier/_actions/check-out.tsx
+++ app/(routes)/(public)/panier/_actions/check-out.tsx
@@ -3,17 +3,13 @@
 import { getSessionUser } from "@/actions/get-user";
 import OrderEmail from "@/components/email/order";
 import Order from "@/components/pdf/create-commande";
-import {
-  createCustomer,
-  createPDFData,
-  generateOrderId,
-} from "@/components/pdf/pdf-data";
+import { createCustomer, createPDFData, generateOrderId } from "@/components/pdf/pdf-data";
 import { getUnitLabel } from "@/components/product/product-function";
 import { dateFormatter, isDateDisabled } from "@/lib/date-utils";
 import { transporter } from "@/lib/nodemailer";
 import prismadb from "@/lib/prismadb";
 import { currencyFormatter } from "@/lib/utils";
-import  type { FullOrder, ProductWithMain, UserWithAddress } from "@/types";
+import type { FullOrder, ProductWithMain, UserWithAddress } from "@/types";
 import { render } from "@react-email/render";
 import { pdf } from "@react-pdf/renderer";
 import { revalidatePath } from "next/cache";
@@ -22,231 +18,217 @@ import * as z from "zod";
 const baseUrl = process.env.NEXT_PUBLIC_URL as string;
 
 type CheckOutReturnType =
-  | {
-      success: true;
-    }
-  | {
-      success: false;
-      message: string;
-      ids?: string[];
-    };
-
-// type CheckOutProps = {
-//   itemsWithQuantities: {
-//     id: string;
-//     price: number;
-//     quantity: number;
-//   }[];
-//   date: Date;
-//   shopId: string;
-// };
+	| {
+			success: true;
+	  }
+	| {
+			success: false;
+			message: string;
+			ids?: string[];
+	  };
 
 const checkOutSchema = z.object({
-  date: z.date().refine((date) => !isDateDisabled(date), {
-    message:
-      "La date n'est pas valide, veuillez selectionner une date correcte",
-  }),
-  shopId: z.string({
-    required_error: "Veuillez selectionner un magasin",
-  }),
-  itemsWithQuantities: z.array(
-    z.object({
-      id: z.string({
-        required_error: "Produit introuvable",
-        invalid_type_error: "Produit introuvable",
-      }),
-      price: z.number({
-        required_error: "Produit introuvable",
-        invalid_type_error: "Prix invalide",
-      }),
-      quantity: z.number({
-        invalid_type_error: "Quantité invalide",
-        required_error: "Quantité invalide",
-      }),
-    }),
-  ),
+	date: z.date().refine((date) => !isDateDisabled(date), {
+		message: "La date n'est pas valide, veuillez selectionner une date correcte",
+	}),
+	shopId: z.string({
+		required_error: "Veuillez selectionner un magasin",
+	}),
+	itemsWithQuantities: z.array(
+		z.object({
+			id: z.string({
+				required_error: "Produit introuvable",
+				invalid_type_error: "Produit introuvable",
+			}),
+			price: z.number({
+				required_error: "Produit introuvable",
+				invalid_type_error: "Prix invalide",
+			}),
+			quantity: z.number({
+				invalid_type_error: "Quantité invalide",
+				required_error: "Quantité invalide",
+			}),
+		}),
+	),
 });
 
 type CheckOutProps = z.infer<typeof checkOutSchema>;
 
-export const checkOut = async ({
-  itemsWithQuantities,
-  date,
-  shopId,
-}: CheckOutProps): Promise<CheckOutReturnType> => {
-  const isAuth = await getSessionUser();
+export const checkOut = async ({ itemsWithQuantities, date, shopId }: CheckOutProps): Promise<CheckOutReturnType> => {
+	const isAuth = await getSessionUser();
 
-  const validated = checkOutSchema.safeParse({
-    date,
-    shopId,
-    itemsWithQuantities,
-  });
+	const validated = checkOutSchema.safeParse({
+		date,
+		shopId,
+		itemsWithQuantities,
+	});
 
-  if (!validated.success) {
-    return {
-      success: false,
-      message: validated.error.issues[0].message,
-    };
-  }
+	if (!validated.success) {
+		return {
+			success: false,
+			message: validated.error.issues[0].message,
+		};
+	}
 
-  if (!isAuth) {
-    return {
-      success: false,
-      message:
-        "Erreur lors de la connexion, reconnectez-vous pour valider votre commande",
-    };
-  }
+	if (!isAuth) {
+		return {
+			success: false,
+			message: "Erreur lors de la connexion, reconnectez-vous pour valider votre commande",
+		};
+	}
 
-  const user = await prismadb.user.findUnique({
-    where: {
-      id: isAuth.id,
-    },
-    include: {
-      address: true,
-      billingAddress: true,
-    },
-  });
+	const user = await prismadb.user.findUnique({
+		where: {
+			id: isAuth.id,
+		},
+		include: {
+			address: true,
+			billingAddress: true,
+		},
+	});
 
-  if (!user) {
-    return {
-      success: false,
-      message:
-        "Erreur lors de la connexion, essaye de vous connecter à nouveau",
-    };
-  }
+	if (!user) {
+		return {
+			success: false,
+			message: "Erreur lors de la connexion, essaye de vous connecter à nouveau",
+		};
+	}
 
-  const productIds = itemsWithQuantities.map((item) => item.id);
-  const products = await prismadb.product.findMany({
-    where: {
-      id: {
-        in: productIds,
-      },
-    },
-    include: {
-      product: true,
-    },
-  });
+	const productIds = itemsWithQuantities.map((item) => item.id);
+	const products = await prismadb.product.findMany({
+		where: {
+			id: {
+				in: productIds,
+			},
+		},
+		include: {
+			product: true,
+		},
+	});
 
-  const productsWithQuantity = products.map((product) => {
-    return {
-      item: product,
-      quantity: itemsWithQuantities.find((item) => item.id === product.id)
-        ?.quantity,
-    };
-  });
+	const productsWithQuantity = products.map((product) => {
+		return {
+			item: product,
+			quantity: itemsWithQuantities.find((item) => item.id === product.id)?.quantity,
+		};
+	});
 
-  const mismatchedProducts = itemsWithQuantities.filter((item) => {
-    const matchingProduct = products.find((product) => product.id === item.id);
-    return !matchingProduct || matchingProduct.price !== item.price;
-  });
+	const mismatchedProducts = itemsWithQuantities.filter((item) => {
+		const matchingProduct = products.find((product) => product.id === item.id);
+		if (!matchingProduct) {
+			return true;
+		}
+		if (matchingProduct.price !== item.price) {
+			return true;
+		}
 
-  const totalPrice = itemsWithQuantities.reduce((acc, { price, quantity }) => {
-    return acc + (price || 0) * (quantity || 1);
-  }, 0);
+		if (matchingProduct.product.isPro && user.role !== "pro") {
+			return true;
+		}
+		return false;
+	});
 
-  if (mismatchedProducts.length > 0) {
-    console.log("Mismatched IDs:", mismatchedProducts);
-    return {
-      success: false,
-      message: `Produits modifiés`,
-      ids: mismatchedProducts.map((product) => product.id),
-    };
-  }
+	const totalPrice = itemsWithQuantities.reduce((acc, { price, quantity }) => {
+		return acc + (price || 0) * (quantity || 1);
+	}, 0);
 
-  try {
-    const order = await createOrder({
-      productsWithQuantity,
-      totalPrice,
-      user,
-      datePickUp: date,
-      shopId,
-    });
-    const pdfBuffer = await generatePdf(order);
+	if (mismatchedProducts.length > 0) {
+		console.log("Mismatched IDs:", mismatchedProducts);
+		return {
+			success: false,
+			message: `Produits modifiés`,
+			ids: mismatchedProducts.map((product) => product.id),
+		};
+	}
 
-    await transporter.sendMail({
-      from: "laiteriedupontrobert@gmail.com",
-      to: user.email || "",
-      subject: "Confirmation de votre commande - Laiterie du Pont Robert",
-      html: render(
-        OrderEmail({
-          date: dateFormatter(order.createdAt),
-          baseUrl,
-          id: order.id,
-          price: currencyFormatter.format(totalPrice),
-        }),
-      ),
-      attachments: [
-        {
-          filename: `Bon_de_commande-${order.id}.pdf`,
-          content: pdfBuffer,
-          contentType: "application/pdf",
-        },
-      ],
-    });
+	try {
+		const order = await createOrder({
+			productsWithQuantity,
+			totalPrice,
+			user,
+			datePickUp: date,
+			shopId,
+		});
+		const pdfBuffer = await generatePdf(order);
 
-    revalidatePath("/dashboard-user/orders");
+		await transporter.sendMail({
+			from: "laiteriedupontrobert@gmail.com",
+			to: user.email || "",
+			subject: "Confirmation de votre commande - Laiterie du Pont Robert",
+			html: render(
+				OrderEmail({
+					date: dateFormatter(order.createdAt),
+					baseUrl,
+					id: order.id,
+					price: currencyFormatter.format(totalPrice),
+				}),
+			),
+			attachments: [
+				{
+					filename: `Bon_de_commande-${order.id}.pdf`,
+					content: pdfBuffer,
+					contentType: "application/pdf",
+				},
+			],
+		});
 
-    return {
-      success: true,
-    };
-  } catch (error) {
-    console.log(error);
-    return {
-      success: false,
-      message: "Erreur lors de la commande.",
-    };
-  }
+		revalidatePath("/dashboard-user/orders");
+
+		return {
+			success: true,
+		};
+	} catch (error) {
+		console.log(error);
+		return {
+			success: false,
+			message: "Erreur lors de la commande.",
+		};
+	}
 };
 
 async function generatePdf(order: FullOrder) {
-  const doc = <Order data={createPDFData(order)} />;
-  const pdfBlob = await pdf(doc).toBlob();
-  const arrayBuffer = await pdfBlob.arrayBuffer();
-  return Buffer.from(arrayBuffer);
+	const doc = <Order data={createPDFData(order)} />;
+	const pdfBlob = await pdf(doc).toBlob();
+	const arrayBuffer = await pdfBlob.arrayBuffer();
+	return Buffer.from(arrayBuffer);
 }
 
 type CreateOrderType = {
-  totalPrice: number;
-  productsWithQuantity: { item: ProductWithMain; quantity?: number }[];
-  shopId: string;
-  user: UserWithAddress;
-  datePickUp: Date;
+	totalPrice: number;
+	productsWithQuantity: { item: ProductWithMain; quantity?: number }[];
+	shopId: string;
+	user: UserWithAddress;
+	datePickUp: Date;
 };
 
-async function createOrder({
-  totalPrice,
-  productsWithQuantity,
-  shopId,
-  user,
-  datePickUp,
-}: CreateOrderType) {
-  const order = await prismadb.order.create({
-    data: {
-      id: generateOrderId(),
-      totalPrice,
-      orderItems: {
-        create: productsWithQuantity.map((product) => ({
-          itemId: product.item.id,
-          name: product.item.name,
-          description: product.item.description,
-          categoryName: product.item.product.categoryName,
-          price: product.item.price,
-          unit: getUnitLabel(product.item.unit).quantity,
-          quantity: product.quantity,
-        })),
-      },
-      userId: user.id,
-      shopId: shopId === "domicile" ? null : shopId,
-      datePickUp,
-      customer: {
-        create: createCustomer(user),
-      },
-    },
-    include: {
-      shop: true,
-      orderItems: true,
-      customer: true,
-    },
-  });
-  return order;
+async function createOrder({ totalPrice, productsWithQuantity, shopId, user, datePickUp }: CreateOrderType) {
+	const order = await prismadb.order.create({
+		data: {
+			id: generateOrderId(),
+			totalPrice,
+			orderItems: {
+				create: productsWithQuantity.map((product) => ({
+					itemId: product.item.id,
+					name: product.item.name,
+					description: product.item.description,
+					categoryName: product.item.product.categoryName,
+					price: product.item.price,
+					unit: getUnitLabel(product.item.unit).quantity,
+					quantity: product.quantity,
+				})),
+			},
+			userId: user.id,
+			shopId: shopId === "domicile" ? null : shopId,
+			datePickUp,
+			customer: {
+				create: createCustomer(user),
+			},
+		},
+		include: {
+			shop: true,
+			orderItems: true,
+			customer: true,
+		},
+	});
+	return order;
 }
