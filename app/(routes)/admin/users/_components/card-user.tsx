e1ce5a9bd3e45a37dbbdb6104eef3601588417e4
Aurélien <aurelien.r35@gmail.com>
2024-07-17 20:57:51 +0200

    center icon

--- app/(routes)/admin/users/_components/card-user.tsx
+++ app/(routes)/admin/users/_components/card-user.tsx
@@ -17,18 +17,10 @@ const CardUser: React.FC<CardUserProps> = ({ user, orderLength, className }) => 
   const name = user.company || user.name || user.email;
   return (
     <>
-      <Card
-        className={cn("flex h-full relative w-[150px] sm:w-[200px] flex-col justify-between bg-transparent", className)}
-      >
-        {/* {user.image ? (
-          <div className=" abosolute inset-0 rounded-md overflow-hidden -z-10 opacity-70">
-            <Image src={user.image} alt="logo" fill className="object-contain" sizes="150px" />
-          </div>
-        ) : null} */}
-        {/* <CardHeaderUser user={user} /> */}
+      <Card className={cn("flex h-full relative w-[150px] sm:w-[200px] flex-col justify-between ", className)}>
         <CardHeader className=" w-full overflow-hidden">
           {user.image ? (
-            <div className="lg:size-32 md:size-28 sm:size-24 size-20 relative rounded-md overflow-hidden">
+            <div className="lg:size-32 md:size-28 sm:size-24 size-20 relative mx-auto rounded-md overflow-hidden">
               <Image src={user.image} alt={`logo ${name}`} fill className="object-contain" sizes="200px" />
             </div>
           ) : (
@@ -43,11 +35,6 @@ const CardUser: React.FC<CardUserProps> = ({ user, orderLength, className }) => 
           )}
         </CardHeader>
         <CardContent className="text-center p-2  rounded-md w-fit mx-auto         ">
-          <div className=" flex justify-center items-center">
-            {/* <p className="flex gap-2">
-              <ListOrdered className="size-6" /> {orderLength}
-            </p> */}
-          </div>
           <div className="flex flex-row items-center justify-center gap-1 font-bold rounded-md">
             {user.role === "pro" ? (
               <p className="text-green-500">Professionnel</p>
