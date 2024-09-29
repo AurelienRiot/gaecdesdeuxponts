ea8987b6accd07f23a21b5224cfcd67c09c11dd4
Aurélien <aurelien.r35@gmail.com>
2024-09-29 17:45:32 +0200

    fix redirect

--- middleware.ts
+++ middleware.ts
@@ -74,7 +74,7 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/admin/:path*", "/profile/:path*", "/"],
+  matcher: ["/admin/:path*", "/profile/:path*"],
 };
 
 const redirectToLogin = (req: NextRequest) =>
