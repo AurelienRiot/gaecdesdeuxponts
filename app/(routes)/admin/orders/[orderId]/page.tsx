0a4f7a1399a12dd9b4f1830f1bd1cfe3f1f53593
Aurélien <aurelien.r35@gmail.com>
2024-10-23 17:01:52 +0200

    new items order

--- app/(routes)/admin/orders/[orderId]/page.tsx
+++ app/(routes)/admin/orders/[orderId]/page.tsx
@@ -5,6 +5,7 @@ import getShippingOrder from "./_functions/get-order";
 import getProductsForOrders from "./_functions/get-products-for-orders";
 import getShopsForOrders from "./_functions/get-shops-for-orders";
 import getUsersForOrders from "./_functions/get-users-for-orders";
+import { getUnitLabel } from "@/components/product/product-function";
 
 export const dynamic = "force-dynamic";
 const OrderFormPage = async ({
@@ -29,6 +30,22 @@ const OrderFormPage = async ({
     getUsersForOrders(),
     getShippingOrder(orderId, dateOfShipping, params.orderId === "new"),
   ]);
+
+  if (initialData && params.orderId === "new") {
+    for (const item of initialData.orderItems) {
+      const product = products.find((product) => product.id === item.itemId);
+      if (product) {
+        item.name = product.name;
+        item.description = product.description;
+        item.categoryName = product.product.categoryName;
+        item.price = product.price;
+        item.stocks = product.stocks.map((stock) => stock.stockId);
+        item.tax = product.tax;
+        item.unit = getUnitLabel(product.unit).quantity;
+      }
+    }
+  }
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-6 p-8 pt-6">
