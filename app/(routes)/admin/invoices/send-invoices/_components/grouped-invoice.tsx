c8e94ee9c3b5fb6a1f7b5f6a2edfbe33068e9f52
Aurélien <aurelien.r35@gmail.com>
2024-11-13 19:36:06 +0100

    chunk of 5

--- app/(routes)/admin/invoices/send-invoices/_components/grouped-invoice.tsx
+++ app/(routes)/admin/invoices/send-invoices/_components/grouped-invoice.tsx
@@ -219,7 +219,7 @@ function GroupedInvoice({ userWithOrdersForInvoices }: { userWithOrdersForInvoic
     setDisplayInvoices(true);
     toast.success("Envoi des factures en cours...");
 
-    const chunkSize = 10;
+    const chunkSize = 5;
     let cumulativeCount = 0;
     const recordLength = Object.keys(invoiceRecord).length;
     for (let i = 0; i < recordLength; i += chunkSize) {
